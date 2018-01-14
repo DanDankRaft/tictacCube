b2cf3133d47fd674eba4b0c24d0c4ce9620bc203
Dankiest Raft <daniel.yahalom@gmail.com>
2018-01-13 18:24:52 -0600

    Testing built in commit from VS

--- Assets/scripts/Placement.cs
+++ Assets/scripts/Placement.cs
@@ -4,37 +4,43 @@ using UnityEngine;
 
 public class Placement : MonoBehaviour {
 
-	public bool wasTouchUsed = false;
 	void Update()
 	{
-		if(Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began))
+		Touch potentialTouch;
+		float touchTime = 0;
+		float touchDistance = 0;
+		if(Input.touchCount > 0)
 		{
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			if(Physics.Raycast(ray, out hit) && hit.collider.gameObject.GetComponent<Slot>() != null)
+			potentialTouch = Input.touches[0];
+			touchTime += potentialTouch.deltaTime;
+			touchDistance += potentialTouch.deltaPosition.magnitude;
+			if(Input.GetMouseButtonDown(0) || (touchTime > 0.1f && touchDistance < 0.5f))
 			{
-				wasTouchUsed = true;
-				Slot ourSlot = hit.collider.gameObject.GetComponent<Slot>();
-				if(ourSlot.getSlotType() == Slot.Type.None)
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+				if(Physics.Raycast(ray, out hit) && hit.collider.gameObject.GetComponent<Slot>() != null)
 				{
-					switch(FindObjectOfType<PlayerManager>().currentPlayer)
+					Slot ourSlot = hit.collider.gameObject.GetComponent<Slot>();
+					if(ourSlot.getSlotType() == Slot.Type.None)
 					{
-						case PlayerManager.Player.X:
-							ourSlot.setSlotType(Slot.Type.X);
-							FindObjectOfType<PlayerManager>().currentPlayer = PlayerManager.Player.O;
-							break;
-						case PlayerManager.Player.O:
-							ourSlot.setSlotType(Slot.Type.O);
-							FindObjectOfType<PlayerManager>().currentPlayer = PlayerManager.Player.X;
-							break;
+						switch(FindObjectOfType<PlayerManager>().currentPlayer)
+						{
+							case PlayerManager.Player.X:
+								ourSlot.setSlotType(Slot.Type.X);
+								FindObjectOfType<PlayerManager>().currentPlayer = PlayerManager.Player.O;
+								break;
+							case PlayerManager.Player.O:
+								ourSlot.setSlotType(Slot.Type.O);
+								FindObjectOfType<PlayerManager>().currentPlayer = PlayerManager.Player.X;
+								break;
+						}
+						FindObjectOfType<PlayerManager>().scoreCalc();
 					}
-					FindObjectOfType<PlayerManager>().scoreCalc();
 				}
+
 			}
-
 		}
+		
 
-		if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Stationary)
-			wasTouchUsed = false;
 	}
 }
