05530a8d1a858ac7e408c154aea82ecb0d1d3099
Dankiest Raft <daniel.yahalom@gmail.com>
2018-01-22 17:23:27 -0600

    Added current player indicator

---
+++ Assets/scripts/playerImage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerImage : MonoBehaviour {
+
+	public Sprite xSprite;
+	public Sprite oSprite;
+
+	void FixedUpdate()
+	{
+		switch(FindObjectOfType<PlayerManager>().currentPlayer)
+		{
+			case PlayerManager.Player.X:
+				GetComponent<UnityEngine.UI.Image>().sprite = xSprite;
+				break;
+			case PlayerManager.Player.O:
+				GetComponent<UnityEngine.UI.Image>().sprite = oSprite;
+				break;
+		}
+	}
+}
