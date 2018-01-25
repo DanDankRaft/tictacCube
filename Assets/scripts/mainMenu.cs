7f6d9c8b745602c9f1672838a8ef13d917a4b5a8
Dankiest Raft <daniel.yahalom@gmail.com>
2018-01-24 22:52:28 -0600

    made the main menu!

---
+++ Assets/scripts/mainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainMenu : MonoBehaviour {
+
+	public void newGame()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene("thelevel");
+	}
+
+}
